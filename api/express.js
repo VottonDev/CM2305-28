ca740cbc2a26ef89b79b7695d2d83ff1e9ae76af
Otto Hooper <votton.hooper@gmail.com>
2021-10-26 14:01:34 +0100

    Simplify API and add node modules

--- api/express.js
+++ api/express.js
@@ -1,7 +1,11 @@
-const app = require('./expressApp');
+const express = require('express');
+const app = express();
+const compression = require('compression')
 
 const port = 3001;
 
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
+
+module.exports = app;
--- api/expressApp.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const compression = require('compression')
-
-
-module.exports = app;
