11e8896869c1289b6b828f363b461a560f95b24f
Otto Hooper <votton.hooper@gmail.com>
2021-10-26 13:53:05 +0100

    Base files for auth (Express) & (Auth route)

---
+++ api/expressApp.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const compression = require('compression')
+
+
+module.exports = app;
---
+++ api/routes/auth.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+var bcrypt = require('bcrypt');
+
+module.exports = app;
