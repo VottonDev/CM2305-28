7a64228753189806ec0a536a55aaa63a09dd963f
Votton <votton.hooper@gmail.com>
2021-11-03 18:22:19 +0000

    Let phpunit pass with @covers

--- api/test/website/Test.php
+++ api/test/website/Test.php
@@ -1,10 +1,9 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 final class StackTest extends TestCase
 {
+    /** @covers testPushAndPop */
     public function testPushAndPop(): void
     {
         $stack = [];
