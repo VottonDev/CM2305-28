390d13836ee6fd0ff21f604b3a0d6eabc094e1db
Votton <votton.hooper@gmail.com>
2021-11-03 19:07:58 +0000

    API Testing & Improvements to PHP Testing (#13)
    * PHPUnit testing fixes and upload to Codecov
    * Jest API testing and upload to Codecov
    * php-cs-fixer added as module
    * prettify php & other files
    * move API listen stuff to a separate file to fix jest testing
    * add few commands to package.json

---
+++ api/test/api/login.test.js
@@ -0,0 +1,9 @@
+const test = require('supertest');
+const app = require('../../express.js');
+
+// Test login
+describe('POST /auth/login', () => {
+  it('should return 200', (done) => {
+    test(app).post('/auth/login').expect(200, done);
+  });
+});
