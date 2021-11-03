b2f44422dffd6cdc37a7f0dbf9f09a1b3db18fbb
Votton <votton.hooper@gmail.com>
2021-11-03 19:07:58 +0000

    API Testing & Improvements to PHP Testing (#13)
    * PHPUnit testing fixes and upload to Codecov
    * Jest API testing and upload to Codecov
    * php-cs-fixer added as module
    * prettify php & other files
    * move API listen stuff to a separate file to fix jest testing
    * add few commands to package.json

--- api/express.js
+++ api/express.js
@@ -1,20 +1,17 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
-const compression = require('compression')
 const auth = require('./routes/auth.js');
 const twitter = require('./routes/twitter.js');
 const flickr = require('./routes/flickr.js');
-const port = 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({
-extended: true
-}));
-app.use(compression())
-
-app.listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`);
-});
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(compression());
 
 app.use('/auth', auth);
 app.use('/twitter', twitter);
