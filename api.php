2495be6f3c4f1ea634f660be09a8fb06c8cf3767
Otto Hooper <votton.hooper@gmail.com>
2021-11-10 15:17:57 +0000

    Fix api.php formatting

--- api.php
+++ api.php
@@ -1,26 +1,27 @@
 <?php
-// Request function to call the NodeJS API
-function request($route, $post_params = null, $get_params = null, $username = "root", $password = "root")
-{
-    $req_url = "localhost:3001" . $route;
 
-    if ($post_params != null) {
+// Request function to call the NodeJS API
+function request($route, $post_params = null, $get_params = null, $username = 'root', $password = 'root')
+{
+    $req_url = 'localhost:3001'.$route;
+
+    if (null != $post_params) {
         $query = http_build_query($post_params, '', '&');
         $data_json = json_encode($_POST);
     }
 
-    if ($get_params != null) {
+    if (null != $get_params) {
         $query = http_build_query($get_params, '', '&');
-        $req_url = $req_url . "/" . $query;
+        $req_url = $req_url.'/'.$query;
     }
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $req_url);
-    if ($post_params != null) {
+    if (null != $post_params) {
         curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/x-www-form-urlencoded'
-        ));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/x-www-form-urlencoded',
+        ]);
     }
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
@@ -33,38 +34,35 @@ function request($route, $post_params = null, $get_params = null, $username = "r
 
 // Register a new user
 if (isset($_POST['register']) && !empty($_POST['register'])) {
-
-    $params = array(
+    $params = [
         'username' => $_POST['username'],
         'password' => $_POST['password'],
-        'confirm_password' => $_POST['confirm_password']
-    );
-    $response = request("/auth/register", $post_params = $params);
-    
-   if ($response == "success") {
-        header("Location: login.php");
-        echo "Account created succesfully.";
+        'confirm_password' => $_POST['confirm_password'],
+    ];
+    $response = request('/auth/register', $post_params = $params);
+
+    if ('success' == $response) {
+        header('Location: login.php');
+        echo 'Account created succesfully.';
     } else {
-        echo "Failed to register.";
-    } 
-}
-
-// Login a user
-if(isset($_POST['login']) && !empty($_POST['login'])){
-    $params = array(
-        'username' => $_POST['username'],
-        'password' => $_POST['password']
-    );
-    $response = request("/auth/login", $post_params = $params);
-
-    $_SESSION['username'] = $response=['username'];
-
-    if ($response == "success") {
-        header("Location: dashboard.php");
-        echo "Login succesful.";
-    } else {
-        echo "Login failed.";
+        echo 'Failed to register.';
     }
 }
 
-?>
+// Login a user
+if (isset($_POST['login']) && !empty($_POST['login'])) {
+    $params = [
+        'username' => $_POST['username'],
+        'password' => $_POST['password'],
+    ];
+    $response = request('/auth/login', $post_params = $params);
+
+    $_SESSION['username'] = $response = ['username'];
+
+    if ('success' == $response) {
+        header('Location: dashboard.php');
+        echo 'Login succesful.';
+    } else {
+        echo 'Login failed.';
+    }
+}
