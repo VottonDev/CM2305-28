a645f97387a41d1df7d30b8fa94969256195f323
Otto Hooper <votton.hooper@gmail.com>
2021-11-26 16:32:41 +0000

    Implement first Twitter API query (Search recent tweets) + install Axios

--- api/routes/twitter.js
+++ api/routes/twitter.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
+const axios = require('axios').default;
+
+// Get recent tweets from twitter
+app.get('/get_recent_tweets', async (req, res) => {
+  let twitter_query = req.body.query;
+  let token = req.body.token;
+  const response = await axios.get('https://api.twitter.com/2/tweets/search/recent', {
+    params: {
+      query: twitter_query,
+    },
+    headers: {
+      Authorization: 'Bearer '+ token,
+    },
+  });
+  res.json(response.data);
+});
 
 module.exports = app;
