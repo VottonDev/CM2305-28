66b566a19f8d551aa5806c8cd7088e2aac30d462
Otto Hooper <votton.hooper@gmail.com>
2021-11-26 17:31:30 +0000

    Write a test for Twitter and Register

--- api/test/api/auth.test.js
+++ api/test/api/auth.test.js
@@ -7,3 +7,11 @@ describe('POST /auth/login', () => {
     test(app).post('/auth/login').expect(200, done);
   });
 });
+
+// Test register
+describe('POST /auth/login', () => {
+  it('should return 200', (done) => {
+    test(app).post('/auth/register')
+    .expect(200, done);
+  });
+});
---
+++ api/test/api/twitter.test.js
@@ -0,0 +1,13 @@
+const test = require('supertest');
+const app = require('../../express.js');
+
+// Test Twitter API with bearer token
+describe('GET /twitter/get_recent_tweets', () => {
+  // Test Twitter API with bearer token
+  it('should return 200', (done) => {
+    test(app)
+      .get('/twitter/get_recent_tweets')
+      .send({ token: process.env.TWITTER_TOKEN, query: 'test' })
+      .expect(200, done);
+  });
+});
