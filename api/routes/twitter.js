c932d538d7516fd616199a8acb42f299187470d9
Otto Hooper <votton.hooper@gmail.com>
2021-12-12 17:16:43 +0000

    Send 401 error if token missing Twitter

--- api/routes/twitter.js
+++ api/routes/twitter.js
@@ -14,18 +14,12 @@ app.get('/get_recent_tweets', async (req, res) => {
       Authorization: 'Bearer '+ token,
     },
   });
-  res.status(200).json(response.data);
-});
-
-// Check that authentication works for Twitter routes
-app.get('/get_recent_tweets', async (req, res) => {
-  let twitter_query = req.body.query;
-  const response = await axios.get('https://api.twitter.com/2/tweets/search/recent', {
-    params: {
-      query: twitter_query,
-    },
-  });
-  res.status(200).json(response.data);
+  if (response.status === 200) {
+    res.status(200).json(response.data);
+  }
+  else {
+    res.status(401).json(response.data);
+  }
 });
 
 module.exports = app;
