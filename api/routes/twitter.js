7b96b5aaa6cc8fef7e7d8716a45ba3eb3bc40899
Otto Hooper <votton.hooper@gmail.com>
2021-12-12 17:45:02 +0000

    Add a Twitter route to fetch location of a tweet

--- api/routes/twitter.js
+++ api/routes/twitter.js
@@ -21,4 +21,20 @@ app.get('/get_recent_tweets', async (req, res) => {
   }
 });
 
+// Twitter fetch tweet's geolocation
+app.get('/get_tweet_geolocation', async (req, res) => {
+  let tweet_id = req.body.tweet_id;
+  let token = req.body.token;
+  const response = await axios.get('https://api.twitter.com/2/tweets/' + tweet_id + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type', {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  });
+  if (response.status === 200) {
+    res.status(200).json(response.data);
+  } else {
+    res.status(401).json(response.data);
+  }
+});
+
 module.exports = app;
