d833d0bb04d08f015a287a93259bb22a649e1ecd
Otto Hooper <votton.hooper@gmail.com>
2022-02-01 00:09:22 +0000

    Fix geolocation route (Hasn't been tested yet)

--- api/routes/flickr.js
+++ api/routes/flickr.js
@@ -4,20 +4,22 @@ const app = express();
 // Flickr get geolocation data of an image      
 app.get('/get_geolocation', (req, res) => {
     let photo_id = req.params.photo_id;
-
-    const response = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation`, {
+    let token = req.params.token;
+    const response = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.geo.getLocation`, 
+    {
         params: {
             query: photo_id,
         },
         headers: {
             Authorization: 'Bearer ' + token,
         },
-    });
-    if (response.status === 200) {
-        res.status(200).json(response.data);
-    } else {
-        res.status(401).json(response.data);
     }
+  );
+  if (response.status === 200) {
+    res.status(200).json(response.data);
+  } else {
+    res.status(401).json(response.data);
+  }
 });
 
 module.exports = app;
