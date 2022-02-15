5e4c9af1e91c2f29cbca7648bafa43dace3d0525
YizhouS <sheny44@cardiff.ac.uk>
2022-02-15 12:33:29 +0000

    change on about us page

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php include_once 'header.php'; ?>
 
-<h1> About </h1>
-<p1> Insert additional info... </p1>
+<h1 class="about"> About </h1>
+<div class="aboutInfo"> We aim to better target the advertising for marketing company through analysing the frequency and preference of products mentioned on social platforms, such as Flicker and Twitter. Our application first identify the geographic areas where sales are below average. Here, we introduce a heat map for illustrating the geographical location of specific photos on Flicker and specific tweets on Twitter, as well as a pie chart for representing the total number of tweets mentioning our product and the competitors'. In this manner, we could evidently discover the market shares of the products, potentially assisting to search the focus of advertising. We next assess the public's affection of our products utilizing VADER. This sentiment analysis tool scores text emotion by searching text keywords on the basis of lexicon and grammar rules. Lastly and the most importantly, our products would be further applied in practice. On the one hand, the potential customers are able to be found based on their interests, followed by establishing their profile including their gender, age, hobbies, and geographical locations. On the other hand, the most watched TV programs from Twitter and the most visited places from Flickr could be seek out, providing valuable suggestions for marketing company to advertising the products. </div>
 
 <?php include_once 'footer.php'; ?>
