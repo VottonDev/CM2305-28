394d0e59f9a9db8c5755af26403bc7c235eb5df1
Votton <votton.hooper@gmail.com>
2022-02-15 02:58:27 +0000

    Fix mapbox, update mapbox and improve dashboard (#55)
    * Update mapbox
    
    * Update PHP composer modules
    
    * Update bootstrap
    
    * Fix data displaying

--- mapConfig.js
+++ mapConfig.js
@@ -1,5 +1,4 @@
-mapboxgl.accessToken =
-        'pk.eyJ1IjoibWFsLXdvb2QiLCJhIjoiY2oyZ2t2em50MDAyMzJ3cnltMDFhb2NzdiJ9.X-D4Wvo5E5QxeP7K_I3O8w';
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFsLXdvb2QiLCJhIjoiY2oyZ2t2em50MDAyMzJ3cnltMDFhb2NzdiJ9.X-D4Wvo5E5QxeP7K_I3O8w';
 
 setupMap()
 
@@ -10,8 +9,26 @@ function setupMap(){
         zoom: 2, //loads map with no zoom
         center: [0,30]
       });
-
      const nav = new mapboxgl.NavigationControl() //adds zoom and rotate btns
      map.addControl(nav)
 
+     map.on('load', () => {
+          map.addSource('sampleData', {
+              'type': 'geojson',
+              'data': './sampleData.geojson'
+          })
+          map.addLayer({
+                 'id': 'sampleData',
+                 'type': 'circle',
+                 'source': 'sampleData',
+                 'paint': {
+                      'circle-radius': {
+                           'base': 1.75,
+                           'stops': [[12, 2], [22, 180]]
+                      },
+                      'circle-color': '#FF0000'
+                 }
+            })
+       }
+     )
 }
