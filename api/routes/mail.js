afd54ccc52af5e38c3d0b24f0644a44983f2207e
Otto Hooper <votton.hooper@gmail.com>
2022-02-18 13:28:17 +0000

    Make a email route for contact us

---
+++ api/routes/mail.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const db = require('../inc/db.js');
+const mail = require('../inc/mail.js');
+
+// Send email route
+app.post('/send_email', (req, res) => {
+    let email = req.body.email;
+    let subject = req.body.subject;
+    let text = req.body.text;
+
+    mail.sendEmail(email, subject, text);
+    res.status(200).send({
+        success: true,
+        message: 'Email sent'
+    });
+});
+
+module.exports = app;
