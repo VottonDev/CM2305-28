070566abae758d65d605df6cc18c05541770d4fd
Otto Hooper <votton.hooper@gmail.com>
2022-03-01 00:14:59 +0000

    Make Twitter tests more readable

--- api/test/api/twitter.test.js
+++ api/test/api/twitter.test.js
@@ -5,36 +5,54 @@ const app = require('../../express.js');
 describe('GET /twitter/get_recent_tweets', () => {
   // Test Twitter API with bearer token
   it('should return 200', (done) => {
-    test(app).get('/twitter/get_recent_tweets').send({ token: process.env.TWITTER_TOKEN, query: 'test' }).expect('Content-Type', /json/).expect(200, done);
+    test(app).get('/twitter/get_recent_tweets').send({
+      token: process.env.TWITTER_TOKEN,
+      query: 'test'
+    }).expect('Content-Type', /json/).expect(200, done);
   });
 });
 
 describe('GET /twitter/get_tweet_geolocation', () => {
   it('should return 200', (done) => {
-    test(app).get('/twitter/get_tweet_geolocation').send({ token: process.env.TWITTER_TOKEN, query: 'test' }).expect('Content-Type', /json/).expect(200, done);
+    test(app).get('/twitter/get_tweet_geolocation').send({
+      token: process.env.TWITTER_TOKEN,
+      query: 'test'
+    }).expect('Content-Type', /json/).expect(200, done);
   });
 });
 
 describe('GET /twitter/search_recent_tweets', () => {
   it('should return 200', (done) => {
-    test(app).get('/twitter/search_recent_tweets').send({ token: process.env.TWITTER_TOKEN, query: 'test' }).expect('Content-Type', /json/).expect(200, done);
+    test(app).get('/twitter/search_recent_tweets').send({
+      token: process.env.TWITTER_TOKEN,
+      query: 'test'
+    }).expect('Content-Type', /json/).expect(200, done);
   });
 });
 
 describe('GET /twitter/get_public_tweets', () => {
   it('should return 200', (done) => {
-    test(app).get('/twitter/get_public_tweets').send({ token: process.env.TWITTER_TOKEN, query: 'test' }).expect('Content-Type', /json/).expect(200, done);
+    test(app).get('/twitter/get_public_tweets').send({
+      token: process.env.TWITTER_TOKEN,
+      query: 'test'
+    }).expect('Content-Type', /json/).expect(200, done);
   });
 });
 
 describe('GET /twitter/get_followers', () => {
   it('should return 200', (done) => {
-    test(app).get('/twitter/get_followers').send({ token: process.env.TWITTER_TOKEN, query: 'test' }).expect('Content-Type', /json/).expect(200, done);
+    test(app).get('/twitter/get_followers').send({
+      token: process.env.TWITTER_TOKEN,
+      query: 'test'
+    }).expect('Content-Type', /json/).expect(200, done);
   });
 });
 
 describe('GET /twitter/get_user_tweets', () => {
   it('should return 200', (done) => {
-    test(app).get('/twitter/get_user_tweets').send({ token: process.env.TWITTER_TOKEN, query: 'test' }).expect('Content-Type', /json/).expect(200, done);
+    test(app).get('/twitter/get_user_tweets').send({
+      token: process.env.TWITTER_TOKEN,
+      query: 'test'
+    }).expect('Content-Type', /json/).expect(200, done);
   });
 });
