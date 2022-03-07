adef77093988b287c73f157e356921b7570503a6
Otot Hooper <votton.hooper@gmail.com>
2022-03-07 23:23:16 +0000

    Make Flickr routes express compatible

--- api/routes/flickr.js
+++ api/routes/flickr.js
@@ -1,130 +1,121 @@
 const Flickr = require('flickr-sdk');
 const flickr = new Flickr(process.env.FLICKR_API_KEY);
+const express = require('express');
+const app = express();
 
 
 // get a list of configured blogs for the calling user
-// GET /api/flickr/blogs
-exports.getBlogs = (req, res) => {
+app.get('/get_blogs', (req, res) => {
     flickr.blogs.getList(req.user.flickr.access_token, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // get the contact list for a user
-// GET /api/flickr/contacts
-exports.getContacts = (req, res) => {
+app.get('/get_contacts', (req, res) => {
     flickr.contacts.getList(req.user.flickr.access_token, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 // return a list of favourite public photos for the given user
-// GET /api/flickr/favorites
-exports.getFavorites = (req, res) => {
+app.get('/get_favourites', (req, res) => {
     flickr.favorites.getList(req.user.flickr.access_token, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // get information about a user
-// GET /api/flickr/user
-exports.getUser = (req, res) => {
+app.get('/get_user', (req, res) => {
     flickr.people.getInfo(req.user.flickr.access_token, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
-// get geolocations data
-// GET /api/flickr/geo
-exports.getGeo = (req, res) => {
+// get geolocations data for a user
+app.get('/get_geo', (req, res) => {
     flickr.places.getInfo(req.user.flickr.access_token, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // return a list of photos matching criteria
-// GET /api/flickr/photos
-exports.getPhotos = (req, res) => {
+app.get('/search_photos', (req, res) => {
     flickr.photos.search(req.user.flickr.access_token, req.query, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // get information about a photo
-// GET /api/flickr/photos/:id
-exports.getPhoto = (req, res) => {
+app.get('/get_photo', (req, res) => {
     flickr.photos.getInfo(req.user.flickr.access_token, req.params.id, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 
 // return the list of people who have favourited a given photo
-// GET /api/flickr/photos/:id/favourites
-exports.getPhotoFavourites = (req, res) => {
+app.get('/get_favourited_people', (req, res) => {
     flickr.photos.getFavorites(req.user.flickr.access_token, req.params.id, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // get comments for a photo
-// GET /api/flickr/photos/:id/comments
-exports.getPhotoComments = (req, res) => {
+app.get('/get_comments', (req, res) => {
     flickr.photos.comments.getList(req.user.flickr.access_token, req.params.id, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // get the geo data for a photo
-// GET /api/flickr/photos/:id/geo
-exports.getPhotoGeo = (req, res) => {
+app.get('/get_photo_geo', (req, res) => {
     flickr.photos.geo.getLocation(req.user.flickr.access_token, req.params.id, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // return a list of photos for the calling user at a specific latitude, longitude and accuracy
-// GET /api/flickr/photos/geo/:lat/:lon/:accuracy
-exports.getPhotosGeo = (req, res) => {
+app.get('/get_photos_at_location', (req, res) => {
     flickr.photos.search(req.user.flickr.access_token, {
         lat: req.params.lat,
         lon: req.params.lon,
@@ -135,18 +126,17 @@ exports.getPhotosGeo = (req, res) => {
         }
         return res.status(200).send(result);
     });
-};
+});
 
 
 // return a list of people in a given photo
-// GET /api/flickr/photos/:id/people
-exports.getPhotoPeople = (req, res) => {
+app.get('/get_people_in_photo', (req, res) => {
     flickr.photos.people.getList(req.user.flickr.access_token, req.params.id, (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         return res.status(200).send(result);
     });
-};
+});
 
 module.exports = app;
