68523b615430d990636f170052e63be78e0cbe55
groomj <groomj@cardiff.ac.uk>
2022-03-08 06:21:22 +0000

    implemented clustering functionality

--- mapConfig.js
+++ mapConfig.js
@@ -14,21 +14,52 @@ function setupMap(){
 
      map.on('load', () => {
           map.addSource('sampleData', {
-              'type': 'geojson',
-              'data': './sampleData.geojson'
-          })
-          map.addLayer({
-                 'id': 'sampleData',
-                 'type': 'circle',
-                 'source': 'sampleData',
-                 'paint': {
-                      'circle-radius': {
-                           'base': 1.75,
-                           'stops': [[12, 2], [22, 180]]
-                      },
-                      'circle-color': '#FF0000'
-                 }
-            })
+              type: 'geojson',
+              data: './sampleData2.geojson', //temp data file. Wider spread of data 
+              cluster: true, //enable clustering
+              clusterMaxZoom: 15,
+              clusterRadius: 40
+             
+              
+          });
+          
+          map.addLayer({ //add clusters and set radius based on no. of data points
+               id: 'clusters',
+               type: 'circle',
+               source: 'sampleData',
+               filter: ['has', 'point_count'],
+               paint:{
+                    'circle-color': '#f1f075',
+                    'circle-radius': ['step', ['get', 'point_count'], 10, 100, 15, 500, 20]                   
+               }
+          });
+
+          map.addLayer({ //add data count for each cluster
+               id: 'cluster-count',
+               type: 'symbol',
+               source: 'sampleData',
+               filter: ['has', 'point_count'],
+               layout: {
+                    'text-field': '{point_count_abbreviated}',
+                    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                    'text-size': 15
+               }
+          });
+
+          map.addLayer({ //add unclustered points (might remove depending on heatmap config)
+               id: 'unclustered_point',
+               type: 'circle',
+               source: 'sampleData',
+               filter: ['!', ['has', 'point_count']],
+               paint: {
+                    'circle-color': '#f1f075',
+                    'circle-radius': 3,
+                    'circle-stroke-width': 0.5,
+                    'circle-stroke-color': '#ffffff'
+               }
+          });
+
+          
        }
      )
 }
