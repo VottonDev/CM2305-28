3648fea016472f784f588c397a5de82164db84b3
Otot Hooper <votton.hooper@gmail.com>
2022-03-08 00:00:11 +0000

    Make twitter BaseURL a dynamic variable

--- api/routes/twitter.js
+++ api/routes/twitter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const axios = require('axios').default;
+const twitter_base_url = 'https://api.twitter.com/2/';
 
 // Get recent tweets from twitter
 app.get('/get_recent_tweets', async (req, res) => {
   let twitter_query = req.body.query;
   let token = req.body.token;
   const response = await axios.get(
-    'https://api.twitter.com/2/tweets/search/recent' + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
+    twitter_base_url + 'tweets/search/recent' + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
     {
       params: {
         query: twitter_query,
@@ -29,7 +30,7 @@ app.get('/get_tweet_geolocation', async (req, res) => {
   let tweet_id = req.body.query;
   let token = req.body.token;
   const response = await axios.get(
-    'https://api.twitter.com/2/tweets/' + tweet_id + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
+    twitter_base_url + 'tweets/' + tweet_id + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
     {
       headers: {
         Authorization: 'Bearer ' + token,
@@ -48,7 +49,7 @@ app.get('/search_recent_tweets', async (req, res) => {
   let twitter_query = req.body.query;
   let token = req.body.token;
   const response = await axios.get(
-    'https://api.twitter.com/2/tweets/search/recent' + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
+    twitter_base_url + 'tweets/search/recent' + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
     {
       params: {
         query: twitter_query,
@@ -70,7 +71,7 @@ app.get('/search_recent_tweets', async (req, res) => {
 //   let twitter_query = req.body.query;
 //   let token = req.body.token;
 //   const response = await axios.get(
-//     'https://api.twitter.com/2/tweets/search/all' + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
+//     twitter_base_url +tweets/search/all' + '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type',
 //     {
 //       params: {
 //         query: twitter_query,
@@ -92,7 +93,7 @@ app.get('/get_followers', async (req, res) => {
   let user_id = req.body.query;
   let token = req.body.token;
   const response = await axios.get(
-    'https://api.twitter.com/2/users/' + user_id + '/following' + '?expansions=pinned_tweet_id',
+    twitter_base_url + 'users/' + user_id + '/following' + '?expansions=pinned_tweet_id',
     {
       headers: {
         Authorization: 'Bearer ' + token,
@@ -111,7 +112,7 @@ app.get('/get_user_tweets', async (req, res) => {
   let user_id = req.body.query;
   let token = req.body.token;
   const response = await axios.get(
-    'https://api.twitter.com/2/users/' + user_id + '/tweets' + '?expansions=',
+    twitter_base_url + 'users/' + user_id + '/tweets' + '?expansions=',
     {
       headers: {
         Authorization: 'Bearer ' + token,
