0c80f31408eb8b5ed0a473375129929c550791ec
Aryan Shetty <93198531+ajshetty27@users.noreply.github.com>
2022-03-09 22:17:11 +0530

    Update Sentiment to create new JSON file
    Updated code that creates a sentiment field in the JSON object for each post and creates a new JSON file called "updated.json".

--- api/sentiment_analyser.js
+++ api/sentiment_analyser.js
@@ -1,19 +1,16 @@
 //install npm install multilang-sentiment
 var sentiment = require('multilang-sentiment');
+const fs = require('fs');
 
 //check if statement is positivie or negative and ouputs statement and its compund score
 function analysis(text){
     console.log();
     var texts = sentiment(text);
-    console.log(texts.score);
-    if(texts.score>0){
-      var score = 'positive statement';
+    if(texts.score>=0){
+      var score = 'positive';
     }
     else if (texts.score < 0){
-      var score = 'negative statement';
-    }
-    else {
-      var score = "neutral"
+      var score = 'negative';
     }
     return score;
 }
@@ -24,6 +21,22 @@ const jsonData= require('./json.json');
 //carry out analysis on each posts in json payload
 for (i = 0; i<jsonData['data'].length; i++){
   var text = jsonData['data'][i]["text"];
-  console.log(text);
-  console.log(analysis(text));
+
+  //Add sentiment field to the json object
+  jsonData['data'][i]['sentiment'] = analysis(text);
 }
+
+console.log(jsonData);
+
+var updatedJson = JSON.stringify(jsonData);
+console.log(updatedJson);
+
+//create updated json file and store data with sentiment field as updated.json
+fs.writeFile("updated.json", updatedJson, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+
+    console.log("JSON file has been saved.");
+});
