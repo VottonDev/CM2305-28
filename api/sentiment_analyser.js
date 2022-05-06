8ed3440e3282b1a3c79a24869c4cf1fa160d406d
Otto Hooper <votton.hooper@gmail.com>
2022-05-06 21:17:03 +0100

    Prettify the files for readability

--- api/sentiment_analyser.js
+++ api/sentiment_analyser.js
@@ -4,86 +4,79 @@ const fs = require('fs');
 const tv_show = require('./tv_shows.js');
 
 //check if statement is positivie or negative and ouputs statement and its compund score
-function analysis(text){
-
-    let score;
-    const texts = sentiment(text);
-    if(texts.score>=0){
-      score = 'positive';
-    }
-    else if (texts.score < 0){
-        score = 'negative';
-    }
-    return score;
+function analysis(text) {
+  let score;
+  const texts = sentiment(text);
+  if (texts.score >= 0) {
+    score = 'positive';
+  } else if (texts.score < 0) {
+    score = 'negative';
+  }
+  return score;
 }
 
 //read in json payload
-const jsonData= require('./json.json');
+const jsonData = require('./json.json');
 
 const prev_data = fs.readFileSync('pulled_data3.geojson');
 const myObject = JSON.parse(prev_data);
 
 // hold all objects to later convert to JSON file
-let text_temp = " ";
-let author_temp = " ";
+let text_temp = ' ';
+let author_temp = ' ';
 geoArray = [];
-global.tempshow  = " ";
-global.geoJSON  = {};
+global.tempshow = ' ';
+global.geoJSON = {};
 
 //carry out analysis on each posts in json payload
 let geoJSON;
 let country_code_temp;
 for (let i = 0; i < jsonData.length; i++) {
+  //created object for geoJSON variable
+  geoJSON = {
+    type: 'Feature',
+    properties: { text: ' ', author_id: ' ', sentiment: ' ', tv_show: ' ' },
+    geometry: { type: 'Point', coordinates: [] },
+  };
 
-    //created object for geoJSON variable
-    geoJSON = {
-        type: "Feature",
-        properties: {text: " ", author_id: " ", sentiment: " ", tv_show: " "},
-        geometry: {type: "Point", coordinates: []}
-    };
+  // add field values to geoJSON object
+  text_temp = jsonData[i].data.text;
+  author_temp = jsonData[i].data.author_id;
+  country_code_temp = jsonData[i].data.country_code;
 
-    // add field values to geoJSON object
-    text_temp = jsonData[i].data.text;
-    author_temp = jsonData[i].data.author_id;
-    country_code_temp = jsonData[i].data.country_code;
+  geoJSON['properties']['text'] = text_temp;
+  geoJSON['properties']['author_id'] = author_temp;
+  //Add sentiment field to the json object
+  //jsonData['data'][i]['sentiment'] = analysis(jsonData['data'][i]["text"]);
+  geoJSON['properties']['sentiment'] = analysis(text_temp);
+  geoJSON['geometry']['coordinates'] = jsonData[0].data.coordinates;
 
-    geoJSON['properties']['text'] = text_temp;
-    geoJSON['properties']['author_id'] = author_temp;
-    //Add sentiment field to the json object
-    //jsonData['data'][i]['sentiment'] = analysis(jsonData['data'][i]["text"]);
-    geoJSON['properties']['sentiment'] = analysis(text_temp);
-    geoJSON['geometry']['coordinates'] = jsonData[0].data.coordinates;
-
-    geoJSON['properties']['tv_show'] = tv_show(country_code_temp).then(show => {
-            geoJSON['properties']['tv_show'] = show;
-            tempshow = show;
-            //console.log(geoJSON);
-        }
-    ).catch(err => {
-            geoJSON['properties']['tv_show'] = " ";
-        }
-    );
-
-
+  geoJSON['properties']['tv_show'] = tv_show(country_code_temp)
+    .then((show) => {
+      geoJSON['properties']['tv_show'] = show;
+      tempshow = show;
+      //console.log(geoJSON);
+    })
+    .catch((err) => {
+      geoJSON['properties']['tv_show'] = ' ';
+    });
 }
 
-setTimeout(function() {
-  for (let i = 0; i<jsonData.length; i++){
+setTimeout(function () {
+  for (let i = 0; i < jsonData.length; i++) {
     geoJSON['properties']['tv_show'] = tempshow;
-      const temp = geoJSON;
-      console.log(temp);
+    const temp = geoJSON;
+    console.log(temp);
     //geoArray.push(temp);
     myObject.push(temp);
-
   }
-    const geoConvert = JSON.stringify(myObject);
-    fs.writeFile("pulled_data3.geojson", geoConvert, 'utf8', function (err) {
+  const geoConvert = JSON.stringify(myObject);
+  fs.writeFile('pulled_data3.geojson', geoConvert, 'utf8', function (err) {
     if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
+      console.log('An error occured while writing JSON Object to File.');
+      return console.log(err);
     }
 
-    console.log("JSON file for geoJSON file has been saved.");
-}
-);
+    console.log('JSON file for geoJSON file has been saved.');
+  });
 }, 2000);
--- api/twitter_check.js
+++ api/twitter_check.js
@@ -1,4 +1,4 @@
-const {TwitterApi} = require('twitter-api-v2');
+const { TwitterApi } = require('twitter-api-v2');
 const fs = require('fs');
 const client = new TwitterApi('AAAAAAAAAAAAAAAAAAAAAL65WAEAAAAA%2Bz3MLAMpuqpuAbdRVV7l3WUMPxU%3DNez0849RCsrNP6MEKDGGZxYlwxENJA6TBhIOcJTW0bzG2wgHJi');
 const axios = require('axios').default;
@@ -7,13 +7,12 @@ const axios = require('axios').default;
 
 let counter = 0;
 
-
-async function get_recent_tweets(jsonArr){
+async function get_recent_tweets(jsonArr) {
   //let token = req.body.token;
   jsonArr = [];
   let token = 'AAAAAAAAAAAAAAAAAAAAAL65WAEAAAAA%2Bz3MLAMpuqpuAbdRVV7l3WUMPxU%3DNez0849RCsrNP6MEKDGGZxYlwxENJA6TBhIOcJTW0bzG2wgHJi';
   const response = await axios.get(
-    "https://api.twitter.com/2/tweets/search/recent" + "?expansions=geo.place_id&tweet.fields=lang,author_id&place.fields=geo,country_code,contained_within&max_results=100",
+    'https://api.twitter.com/2/tweets/search/recent' + '?expansions=geo.place_id&tweet.fields=lang,author_id&place.fields=geo,country_code,contained_within&max_results=100',
     {
       params: {
         query: 'Holiday',
@@ -27,42 +26,42 @@ async function get_recent_tweets(jsonArr){
     //res.status(200).json(response.data);
     //console.log(response.data.includes.places[0].geo.bbox);
     //console.log(response.data.includes.places[0].country_code);
-    for (i=0; i<100; i++){
-        if (response.data.data[i].geo != null){
-          const payload = {
-            data:{text: " ", author_id: " ", coordinates:" ", country_code: " "}
-          };
-          //console.log(response.data.data[i].text);
-          payload.data.text = response.data.data[i].text;
-          //console.log(response.data.data[i].author_id);
-          payload.data.author_id = response.data.data[i].author_id;
-          counter += 1;
-          //console.log(response.data.includes.places[counter-1].geo.bbox);
-          payload.data.coordinates = response.data.includes.places[counter-1].geo.bbox;
-          //console.log(response.data.includes.places[counter-1].country_code);
-          payload.data.country_code = response.data.includes.places[counter-1].country_code;
+    for (i = 0; i < 100; i++) {
+      if (response.data.data[i].geo != null) {
+        const payload = {
+          data: { text: ' ', author_id: ' ', coordinates: ' ', country_code: ' ' },
+        };
+        //console.log(response.data.data[i].text);
+        payload.data.text = response.data.data[i].text;
+        //console.log(response.data.data[i].author_id);
+        payload.data.author_id = response.data.data[i].author_id;
+        counter += 1;
+        //console.log(response.data.includes.places[counter-1].geo.bbox);
+        payload.data.coordinates = response.data.includes.places[counter - 1].geo.bbox;
+        //console.log(response.data.includes.places[counter-1].country_code);
+        payload.data.country_code = response.data.includes.places[counter - 1].country_code;
 
-            const temp = payload;
-            //console.log(temp);
+        const temp = payload;
+        //console.log(temp);
 
-          jsonArr.push(temp);
+        jsonArr.push(temp);
 
-          //console.log(payload);
-        }
+        //console.log(payload);
+      }
     }
     //console.log(json);
   } else {
     //res.status(401).json(response.data);
   }
-    let main_json = JSON.stringify(jsonArr);
+  let main_json = JSON.stringify(jsonArr);
   console.log(jsonArr);
-  fs.writeFile("json.json", main_json, 'utf8', function (err) {
-      if (err) {
-          console.log("An error occured while writing JSON Object to File.");
-          return console.log(err);
-      }
+  fs.writeFile('json.json', main_json, 'utf8', function (err) {
+    if (err) {
+      console.log('An error occured while writing JSON Object to File.');
+      return console.log(err);
+    }
 
-      console.log("JSON file has been saved.");
+    console.log('JSON file has been saved.');
   });
 }
 
