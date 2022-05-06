28bc22768769849a3c3b1a978b2aebc8380609c1
Otto Hooper <votton.hooper@gmail.com>
2022-05-06 18:56:18 +0100

    TV fetching and sentiment updating

--- api/tv_shows.js
+++ api/tv_shows.js
@@ -1,16 +1,27 @@
-const Trakt = require('trakt-api');
-const trakt = Trakt(process.env.TRAKT_API_KEY);
+const Trakt = require('trakt.tv');
+var Promise = require("promise");
+require('dotenv').config();
 
-const country = 'us';
+let options = {
+    client_id: process.env.TRAKT_CLIENT_ID,
+    client_secret: process.env.TRAKT_CLIENT_SECRET,
+};
 
-trakt
-  .showTrending({ countries: country, extended: 'full' })
-  .then(function (show) {
-    console.log('list of trending tv shows in specified country: ');
-    for (let i = 0; i < show.length; i++) {
-      console.log(show[i]['show']['title']);
+function getTrendingTvShow(country) {
+    return new Promise((resolve, reject) => {
+        let trakt = new Trakt(options);
+        trakt.shows.trending(country)
+            .then(shows => {
+                let randomIndex = Math.floor(Math.random() * shows.length);
+                let randomShow = shows[randomIndex].show.title;
+                resolve(randomShow);
+            }
+            ).catch(err => {
+                reject(err);
+            }
+            );
     }
-  })
-  .catch(function (err) {
-    console.warn('error', err);
-  });
+    );
+}
+
+module.exports  = getTrendingTvShow;
