82a9fb5008722333873cfd05364eeccca571003c
Otto Hooper <votton.hooper@gmail.com>
2022-05-06 21:17:03 +0100

    Prettify the files for readability

--- api/tv_shows.js
+++ api/tv_shows.js
@@ -1,27 +1,26 @@
 const Trakt = require('trakt.tv');
-const Promise = require("promise");
+const Promise = require('promise');
 require('dotenv').config();
 
 let options = {
-    client_id: process.env.TRAKT_CLIENT_ID,
-    client_secret: process.env.TRAKT_CLIENT_SECRET,
+  client_id: process.env.TRAKT_CLIENT_ID,
+  client_secret: process.env.TRAKT_CLIENT_SECRET,
 };
 
 function getTrendingTvShow(country) {
-    return new Promise((resolve, reject) => {
-        let trakt = new Trakt(options);
-        trakt.shows.trending(country)
-            .then(shows => {
-                let randomIndex = Math.floor(Math.random() * shows.length);
-                let randomShow = shows[randomIndex].show.title;
-                resolve(randomShow);
-            }
-            ).catch(err => {
-                reject(err);
-            }
-            );
-    }
-    );
+  return new Promise((resolve, reject) => {
+    let trakt = new Trakt(options);
+    trakt.shows
+      .trending(country)
+      .then((shows) => {
+        let randomIndex = Math.floor(Math.random() * shows.length);
+        let randomShow = shows[randomIndex].show.title;
+        resolve(randomShow);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
-module.exports  = getTrendingTvShow;
+module.exports = getTrendingTvShow;
