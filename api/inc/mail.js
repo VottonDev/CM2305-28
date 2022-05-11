ef14c44c744ddfce1813810a939c34088059abab
Otto Hooper <votton.hooper@gmail.com>
2022-05-11 02:21:37 +0100

    Revert "Revert "Merge branch 'main' into mapStuff""
    This reverts commit 78c2e131b1c91a9f433b35f4ff5c5bcfb4a5b6e0.

--- api/inc/mail.js
+++ api/inc/mail.js
@@ -1,35 +1,35 @@
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: false,
-        ciphers: 'SSLv3'
-    }
+  host: process.env.MAIL_HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+    ciphers: 'SSLv3',
+  },
 });
 
 function sendEmail(email, subject, text) {
-    let mailOptions = {
-        from: '',
-        to: email,
-        subject: subject,
-        text: text
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+  let mailOptions = {
+    from: '',
+    to: email,
+    subject: subject,
+    text: text,
+  };
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
 
 module.exports = {
-    sendEmail: sendEmail
-}
+  sendEmail: sendEmail,
+};
