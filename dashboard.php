b1bff81727d6b1db91bf2745df158d4a1503f0d6
Otto Hooper <votton.hooper@gmail.com>
2022-05-13 20:35:29 +0100

    Calculate tv shows and products

--- dashboard.php
+++ dashboard.php
@@ -25,6 +25,34 @@
             ++$sentiment['neutral'];
         }
     }
+
+    // Get the tv_show from properties and count how many times each tv show is mentioned
+    $tv_show_names = [];
+    $tv_show_count = [];
+
+    for ($i = 0; $i < $totalPosts; ++$i) {
+        $tv_show_name = $json_data['features'][$i]['properties']['tv_show'];
+        if (!(in_array($tv_show_name, $tv_show_names))) {
+            $tv_show_names[] = $tv_show_name;
+            $tv_show_count[] = 1;
+        } else {
+            $tv_show_count[array_search($tv_show_name, $tv_show_names)] += 1;
+        }
+    }
+
+    // Get the product from properties and count how many times each product is mentioned
+    $product_names = [];
+    $product_count = [];
+
+    for ($i = 0; $i < $totalPosts; ++$i) {
+        $product_name = $json_data['features'][$i]['properties']['product'];
+        if (!(in_array($product_name, $product_names))) {
+            $product_names[] = $product_name;
+            $product_count[] = 1;
+        } else {
+            $product_count[array_search($product_name, $product_names)] += 1;
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
