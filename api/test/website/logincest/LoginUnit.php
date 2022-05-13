3112f0b4e8b5998cbdb036a920dce97a069e2faf
Otto Hooper <votton.hooper@gmail.com>
2022-05-13 21:48:06 +0100

    Build test suite

---
+++ api/test/website/logincest/LoginUnit.php
@@ -0,0 +1,18 @@
+<?php
+class UserTest extends \Codeception\Test\Unit
+{
+    public function testValidation()
+    {
+        $user = new User();
+
+        $user->setName(null);
+        $this->assertFalse($user->validate(['username']));
+
+        $user->setName('toolooooongnaaaaaaameeee');
+        $this->assertFalse($user->validate(['username']));
+
+        $user->setName('davert');
+        $this->assertTrue($user->validate(['username']));
+    }
+}
+?>
