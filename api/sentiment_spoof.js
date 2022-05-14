c78c5006a8ed7474e55a4f49f182ee744c9bf441
Aryan Shetty <93198531+ajshetty27@users.noreply.github.com>
2022-05-14 22:02:51 +0530

    Add files via upload

--- api/sentiment_spoof.js
+++ api/sentiment_spoof.js
@@ -1,41 +1,44 @@
-const sentiment = require('multilang-sentiment');
+var sentiment = require('multilang-sentiment');
 const fs = require('fs');
 
 //check if statement is positivie or negative and ouputs statement and its compund score
-function analysis(text) {
-  let score;
-  const texts = sentiment(text);
-  if (texts.score >= 0) {
-    score = 'positive';
-  } else if (texts.score < 0) {
-    score = 'negative';
-  }
-  return score;
+function analysis(text){
+
+    var texts = sentiment(text);
+    if(texts.score>=0){
+      var score = 'positive';
+    }
+    else if (texts.score < 0){
+      var score = 'negative';
+    }
+    return score;
 }
 
 //read in json payload
-const jsonData = require('./json.json');
+const jsonData= require('./json.json');
 
-const prev_data = fs.readFileSync('pulled_data_load.geojson');
-const myObject = JSON.parse(prev_data);
+var prev_data = fs.readFileSync('pulled_data_load.geojson');
+var myObject = JSON.parse(prev_data);
 
 // hold all objects to later convert to JSON file
-let text_temp = ' ';
-let author_temp = ' ';
+var text_temp = " "
+var author_temp = " "
 geoArray = [];
-tv_array = ['Better Call Saul', 'Ozark', 'FBI', 'Moon Knight', 'Halo', 'This Is Us', 'Star Trek: Picard', 'MasterChef Australia', 'New Amsterdam', 'The Resident'];
+tv_array = ["Better Call Saul","Ozark","FBI","Moon Knight","Halo","This Is Us","Star Trek: Picard","MasterChef Australia","New Amsterdam","The Resident"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+
 //carry out analysis on each posts in json payload
-for (let i = 0; i < jsonData.length; i++) {
+for (i = 0; i<jsonData.length; i++){
+
   //created object for geoJSON variable
   geoJSON = {
-    type: 'Feature',
-    properties: { text: ' ', author_id: ' ', sentiment: ' ', tv_show: ' ', product: ' ' },
-    geometry: { type: 'Point', coordinates: [] },
+    type: "Feature",
+    properties:{text: " ", author_id: " ", sentiment: " ", tv_show: " ", product:" ", country_code:" ", source: " ", retweets:" ", likes:" "},
+    geometry: {type: "Point", coordinates: []}
   };
 
   // add field values to geoJSON object
@@ -44,6 +47,10 @@ for (let i = 0; i < jsonData.length; i++) {
   country_code_temp = jsonData[i].data.country_code;
   coordinates = jsonData[i].data.coordinates;
   product_temp = jsonData[i].data.product;
+  source_temp = jsonData[i].data.source;
+  retweets_temp  = jsonData[i].data.retweets;
+  likes_temp = jsonData[i].data.likes;
+
 
   geoJSON['properties']['text'] = text_temp;
   geoJSON['properties']['author_id'] = author_temp;
@@ -52,6 +59,10 @@ for (let i = 0; i < jsonData.length; i++) {
   geoJSON['properties']['sentiment'] = analysis(text_temp);
   geoJSON['geometry']['coordinates'] = coordinates;
   geoJSON['properties']['product'] = product_temp;
+  geoJSON['properties']['country_code'] = country_code_temp;
+  geoJSON['properties']['source'] = source_temp;
+  geoJSON['properties']['retweets'] = retweets_temp;
+  geoJSON['properties']['likes'] = likes_temp;
 
   //console.log(jsonData[0].data.coordinates);
 
@@ -59,14 +70,17 @@ for (let i = 0; i < jsonData.length; i++) {
 
   myObject.push(geoJSON);
   console.log(geoJSON);
+
+
 }
 
-const geoConvert = JSON.stringify(myObject);
-fs.writeFile('pulled_data_load.geojson', geoConvert, 'utf8', function (err) {
-  if (err) {
-    console.log('An error occured while writing JSON Object to File.');
-    return console.log(err);
-  }
 
-  console.log('JSON file for geoJSON file has been saved.');
-});
+  var geoConvert = JSON.stringify(myObject);
+  fs.writeFile("pulled_data_load.geojson", geoConvert, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+
+    console.log("JSON file for geoJSON file has been saved.");
+  });
