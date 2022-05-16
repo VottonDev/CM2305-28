d6b2d07299fb8b2b9442c192ade1c9e2d76d0b69
Otto Hooper <votton.hooper@gmail.com>
2022-05-16 16:38:34 +0100

    Prettify files

--- api/copy_file.js
+++ api/copy_file.js
@@ -4,11 +4,11 @@ const prev_data = fs.readFileSync('pulled_data_load.geojson');
 
 geoConvert = prev_data;
 
-fs.writeFile("pulled_data_load.geojson", geoConvert, 'utf8', function (err) {
+fs.writeFile('pulled_data_load.geojson', geoConvert, 'utf8', function (err) {
   if (err) {
-      console.log("An error occured while writing JSON Object to File.");
-      return console.log(err);
+    console.log('An error occured while writing JSON Object to File.');
+    return console.log(err);
   }
 
-  console.log("geoJSON file has been saved.");
+  console.log('geoJSON file has been saved.');
 });
