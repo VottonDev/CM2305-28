855aaec6e3c171c27e28d2ef013ff45c235a505c
Otto Hooper <votton.hooper@gmail.com>
2022-05-16 16:38:34 +0100

    Prettify files

--- api/twitter_check.js
+++ api/twitter_check.js
@@ -11,7 +11,8 @@ async function get_recent_tweets(jsonArr) {
   jsonArr = [];
   let token = process.env.TWITTER_TOKEN;
   const response = await axios.get(
-    'https://api.twitter.com/2/tweets/search/recent' + "?expansions=geo.place_id&tweet.fields=context_annotations,public_metrics,lang,author_id,source&place.fields=geo,country_code,contained_within&max_results=100",
+    'https://api.twitter.com/2/tweets/search/recent' +
+      '?expansions=geo.place_id&tweet.fields=context_annotations,public_metrics,lang,author_id,source&place.fields=geo,country_code,contained_within&max_results=100',
     {
       params: {
         query: 'Holiday',
@@ -28,19 +29,21 @@ async function get_recent_tweets(jsonArr) {
     for (i = 0; i < 100; i++) {
       if (response.data.data[i].geo != null && 'context_annotations' in response.data.data[i]) {
         const payload = {
-          data: { text: ' ', author_id: ' ', coordinates: ' ', country_code: ' ', source:" ", retweets:" ", likes:" ", interests:" " },
+          data: { text: ' ', author_id: ' ', coordinates: ' ', country_code: ' ', source: ' ', retweets: ' ', likes: ' ', interests: ' ' },
         };
         //console.log(response.data.data[i].text);
-            counter += 1;
-            payload.data.interests = response.data.data[i].context_annotations[0].domain.description;
-            payload.data.text = response.data.data[i].text;
-            payload.data.author_id = response.data.data[i].author_id;
-            payload.data.coordinates = [(response.data.includes.places[counter-1].geo.bbox[2]-response.data.includes.places[counter-1].geo.bbox[0])/2,
-          (response.data.includes.places[counter-1].geo.bbox[3]-response.data.includes.places[counter-1].geo.bbox[1])/2];
-            payload.data.country_code = response.data.includes.places[counter-1].country_code;
-            payload.data.source = response.data.data[i].source;
-            payload.data.retweets = response.data.data[i].public_metrics.retweet_count;
-            payload.data.likes = response.data.data[i].public_metrics.like_count;
+        counter += 1;
+        payload.data.interests = response.data.data[i].context_annotations[0].domain.description;
+        payload.data.text = response.data.data[i].text;
+        payload.data.author_id = response.data.data[i].author_id;
+        payload.data.coordinates = [
+          (response.data.includes.places[counter - 1].geo.bbox[2] - response.data.includes.places[counter - 1].geo.bbox[0]) / 2,
+          (response.data.includes.places[counter - 1].geo.bbox[3] - response.data.includes.places[counter - 1].geo.bbox[1]) / 2,
+        ];
+        payload.data.country_code = response.data.includes.places[counter - 1].country_code;
+        payload.data.source = response.data.data[i].source;
+        payload.data.retweets = response.data.data[i].public_metrics.retweet_count;
+        payload.data.likes = response.data.data[i].public_metrics.like_count;
 
         const temp = payload;
         //console.log(temp);
